cc8f358c5fee787cb68bb88d72e8f55e2bf9bfd4
Noemi <ryfnoe01@students.zhaw.ch>
2023-05-18 22:50:49 +0200

    1 commit

---
+++ src/main/java/ch/zhaw/mdm/demo/NeuralMachineTranslationApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.mdm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuralMachineTranslationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuralMachineTranslationApplication.class, args);
+	}
+
+}
