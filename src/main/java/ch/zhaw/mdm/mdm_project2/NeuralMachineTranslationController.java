daee79e148eced0c20064f60e2039e42c15eefea
Noemi <ryfnoe01@students.zhaw.ch>
2023-05-20 09:47:42 +0200

    Controller Update

--- src/main/java/ch/zhaw/mdm/mdm_project2/NeuralMachineTranslationController.java
+++ src/main/java/ch/zhaw/mdm/mdm_project2/NeuralMachineTranslationController.java
@@ -18,21 +18,37 @@ public class NeuralMachineTranslationController {
     @Autowired
     private NeuralMachineTranslationService translation;
 
+    /**
+     * Handles the GET request for the root URL ("/").
+     * @return The name of the view template to render (index.html in this case).
+     */
     @GetMapping("/")
     public String index() {
         return "index";
     }
 
+    /**
+     * Handles the POST request for the "/translate" URL.
+     * @param frenchText The French text to translate.
+     * @param model The model object to add attributes for rendering the view.
+     * @return The name of the view template to render (index.html in this case).
+     */
     @PostMapping("/translate")
     public String translate(
             @RequestParam(name = "frenchText", required = false) String frenchText,
             Model model) {
         try {
+            // Translate the French text to English using the translation service.
             String englishText = translation.translateText(frenchText);
+            
+            // Add the translated English text as an attribute to the model for rendering.
             model.addAttribute("englishText", englishText);
         } catch (ModelException | TranslateException | IOException e) {
+            // Handle any exceptions that occur during translation and add an error attribute to the model.
             model.addAttribute("error", "Translation failed.");
         }
+        
+        // Return the name of the view template to render (index.html in this case).
         return "index";
     }
 }
